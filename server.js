be50772684ee70ff5f5c7818fbafae0e7481cb10
march-dave <ysangy@gmail.com>
2018-05-05 18:32:52 -0400

    template

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 import express from 'express';
 import apiRouter from './api';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
 
 const server = express();
+server.use(morgan('dev'));
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }));
 const port = process.env.port || 8080;
 
 server.set('view engine', 'ejs');
@@ -9,6 +14,7 @@ server.set('view engine', 'ejs');
 server.get('/', (req, res) => {
     res.render('index');
 });
+
 
 server.use('/api', apiRouter);
 server.use(express.static('public'));
--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,16 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 class App extends Component {
+
+    state = {
+        person: []
+    }
+
+    componentDidMount(){
+        const result = axios.get('http://localhost:8080/api');
+        this.setState({person: result.data});
+    }
+
     render() {
         return (
             <div>React JS Start Note</div>
