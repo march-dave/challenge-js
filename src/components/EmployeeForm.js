7ebe888d072cc705210f4de8a6ce9742834b1e5e
march-dave <ysangy@gmail.com>
2018-05-09 19:58:24 -0400

    up to date

--- src/components/EmployeeForm.js
+++ src/components/EmployeeForm.js
@@ -12,13 +12,9 @@ class EmployeeForm extends Component {
     });
   }
   handleSubmit = (e) => {
-    // submit 버튼은 기본적으로 페이지를 새로 불러오게됨
-    // 그렇게 되면 유저 인터랙션을 모두 놓쳐버리니까 이 함수를 사용하여
-    // 원래해야 하는 작업을 방지함
     e.preventDefault();
-    // 상태값을 onCreate 를 통하여 부모에게 전달
     this.props.onCreate(this.state);
-    // 상태 초기화
+    
     this.setState({
       name: '',
       phone: ''
--- src/components/EmployeeInfo.js
+++ src/components/EmployeeInfo.js
@@ -10,31 +10,21 @@ class EmployeeInfo extends Component {
   };
 
   state = {
-    // 우리는 수정 버튼을 눌렀을 떄 editing 값을 true 로 설정해줄것입니다.
-    // 이 값이 true 일 때에는, 기존에 텍스트 형태로 보여주던 값들을
-    // input 형태로 보여주게 됩니다.
     editing: false,
-    // input 의 값은 유동적이겠지요? input 값을 담기 위해서 각 필드를 위한 값도
-    // 설정합니다
     name: '',
     phone: ''
   };
 
   handleRemove = () => {
-    // 삭제 버튼이 클릭되면 onRemove 에 id 넣어서 호출
     const { info, onRemove } = this.props;
     onRemove(info.id);
   };
 
-  // editing 값을 반전시키는 함수입니다
-  // true -> false, false -> true
   handleToggleEdit = () => {
     const { editing } = this.state;
     this.setState({ editing: !editing });
   };
 
-  // input 에서 onChange 이벤트가 발생 될 때
-  // 호출되는 함수입니다
   handleChange = e => {
     const { name, value } = e.target;
     this.setState({
@@ -43,14 +33,9 @@ class EmployeeInfo extends Component {
   };
 
   componentDidUpdate(prevProps, prevState) {
-    // 여기서는, editing 값이 바뀔 때 처리 할 로직이 적혀있습니다.
-    // 수정을 눌렀을땐, 기존의 값이 input에 나타나고,
-    // 수정을 적용할땐, input 의 값들을 부모한테 전달해줍니다.
 
     const { info, onUpdate } = this.props;
     if (!prevState.editing && this.state.editing) {
-      // editing 값이 false -> true 로 전환 될 때
-      // info 의 값을 state 에 넣어준다
       this.setState({
         name: info.name,
         phone: info.phone
@@ -58,7 +43,6 @@ class EmployeeInfo extends Component {
     }
 
     if (prevState.editing && !this.state.editing) {
-      // editing 값이 true -> false 로 전환 될 때
       onUpdate(info.id, {
         name: this.state.name,
         phone: this.state.phone
@@ -67,13 +51,11 @@ class EmployeeInfo extends Component {
   }
 
   shouldComponentUpdate(nextProps, nextState) {
-    // 수정 상태가 아니고, info 값이 같다면 리렌더링 안함
     if (!this.state.editing  
         && !nextState.editing
         && nextProps.info === this.props.info) {
       return false;
     }
-    // 나머지 경우엔 리렌더링함
     return true;
   }
 
@@ -88,7 +70,7 @@ class EmployeeInfo extends Component {
     const { editing } = this.state;
 
     
-    if (editing) { // 수정모드
+    if (editing) { // Edit mode
       return (
         <div style={style}>
           <div>
@@ -114,7 +96,7 @@ class EmployeeInfo extends Component {
     }
 
 
-    // 일반모드
+    // Readonly mode
     const {
       name, phone
     } = this.props.info;
